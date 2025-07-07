1fbdc70d145e63c55aa212888fc259abd6d88573
Jamie Summerow <jamiesummerow@gmail.com>
2025-07-07 01:00:11 -0400

    second commit
    first commit didnt show issues with terminal

---
+++ Stats.jsx
@@ -0,0 +1,19 @@
+const Stats = () => {
+  return (
+    <section className="stats">
+      <div className="container">
+        {/* Stat Title */}
+        <div className="stats__title">
+          <h2>Advanced Statistics</h2>
+          <p className="p_title">
+            Track how your links are performing across the web with our advanced
+            statistics dashboard.
+          </p>
+        </div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
